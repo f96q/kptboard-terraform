6e934cc00fdb48c2624aa473a1a1222248d7b2e7
danny <danny.dev8@gmail.com>
2017-11-10 10:31:52 +0900

    Fix recent ecs optimized ami

--- aws_instance.tf
+++ aws_instance.tf
@@ -6,8 +6,38 @@ data "template_file" "aws_instance_kptboard_user_data" {
   }
 }
 
+data "aws_ami" "ecs_optimized" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
+
 resource "aws_instance" "kptboard" {
-  ami = "${lookup(var.aws_ecs_optimized_ami_ids, data.aws_region.current.name)}"
+  ami = "${data.aws_ami.ecs_optimized.id}"
   instance_type = "${var.aws_instance_kptboard_instance_type}"
   iam_instance_profile = "${aws_iam_instance_profile.kptboard_ec2.name}"
   security_groups = ["${aws_security_group.kptboard_ecs.name}"]
--- variable.tf
+++ variable.tf
@@ -2,23 +2,6 @@ variable "kptboard" {
   default = "kptboard"
 }
 
-# http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
-variable "aws_ecs_optimized_ami_ids" {
-  default = {
-    "us-east-2"      = "ami-34032e51"
-    "us-east-1"      = "ami-ec33cc96"
-    "us-west-2"      = "ami-29f80351"
-    "us-west-1"      = "ami-d5d0e0b5"
-    "eu-west-2"      = "ami-eb62708f"
-    "eu-west-1"      = "ami-13f7226a"
-    "eu-central-1"   = "ami-40d5672f"
-    "ap-northeast-1" = "ami-21815747"
-    "ap-southeast-2" = "ami-4f08e82d"
-    "ap-southeast-1" = "ami-99f588fa"
-    "ca-central-1"   = "ami-9b54edff"
-  }
-}
-
 variable "aws_instance_kptboard_instance_type" {
   default = "t2.micro"
 }
